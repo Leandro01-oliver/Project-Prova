5bf1b71ae590788a4246b8f397ed9abd2ac024ab
LeandroX01 <78614435+Leandro01-oliver@users.noreply.github.com>
2022-06-05 09:56:52 -0400

    Ajuste no menu logo e user

---
+++ config/connectingFirebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAXw8bkbdeP4X17c741SFk1n6Jnu-qej9w",
+    authDomain: "generation-card.firebaseapp.com",
+    projectId: "generation-card",
+    storageBucket: "generation-card.appspot.com",
+    messagingSenderId: "867201068003",
+    appId: "1:867201068003:web:1b7c76ad7ad6a60c8f0ed6",
+    measurementId: "G-CC38ZQQVV7"
+};
+
+  const app = initializeApp(firebaseConfig);
+
+  const db = getFirestore(app);
+  const auth  = getAuth(app);
+  const storage = getStorage(app);
+
+  export{ db, auth, storage}
